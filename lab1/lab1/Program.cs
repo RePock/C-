4a362065006e31e51868e1beca77c5cf7223b535
RePock <mikov08.08@gmail.com>
2019-02-17 19:02:37 +0700

    lab

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace lab1 {
+    internal class Program {
+        private static void Main() {
+            Console.WriteLine("Enter date:");
+            var inputDate = Console.ReadLine();
+            var calendar = new Calendar(inputDate);
+            calendar.Show();
+            Console.ReadKey();
+        }
+    }
+
+    internal class Calendar {
+        private readonly string _date;
+        private const int DayInWeek = 7;
+        private const string RightOffsetOneDigit = "    ";
+        private const string RightOffsetTwoDigit = "   ";
+        private const string LeftOffset = "   ";
+        private const string StartOffset = "   ";
+        private const string Delimiter = "|";
+
+        public Calendar(string date) {
+            _date = date;
+        }
+
+        public void Show() {
+            if (!ParseDate(_date, out var date)) {
+                return;
+            }
+
+            ShowNamesOfDays(date);
+            ShowCalendar(date, out var workingDays);
+            ShowWorkingDays(workingDays);
+        }
+
+        private static bool ParseDate(string inputDate, out DateTime date) {
+            if (DateTime.TryParse(inputDate, out date)) {
+                return true;
+            }
+
+            Console.WriteLine("Wrong input. Please, use one of these formats:" + "\n05/01/2009 14:57:32.8" +
+                              "\n2009-05-01 14:57:32.8" + "\n2009-05-01T14:57:32.8375298-04:00" + "\n5/01/2008" +
+                              "\n5/01/2008 14:57:32.80 -07:00" + "\n1 May 2008 2:57:32.8 PM" +
+                              "\n16-05-2009 1:00:32 PM" + "\nFri, 15 May 2009 20:10:57 GMT" + "\n10.05.2019" +
+                              "\n10.05.19" + "10.05");
+            return false;
+        }
+
+        private static void ShowNamesOfDays(DateTime date) {
+            Console.WriteLine($"Calendar for {date:MMMM} {date.Year}");
+            Console.Write(StartOffset);
+            var curDate = new DateTime(date.Year, date.Month, 1);
+            while (curDate.DayOfWeek != DayOfWeek.Monday) {
+                curDate = curDate.AddDays(1);
+            }
+
+            do {
+                Console.Write($"{curDate:ddd}" + RightOffsetTwoDigit + Delimiter + LeftOffset);
+                curDate = curDate.AddDays(1);
+            } while (curDate.DayOfWeek != DayOfWeek.Monday);
+
+            Console.WriteLine();
+        }
+
+        private static void ShowCalendar(DateTime date, out int countOfWorkingDays) {
+            countOfWorkingDays = 0;
+            var currentDate = new DateTime(date.Year, date.Month, 1);
+            SkipDays(currentDate);
+            while (currentDate.Month == date.Month) {
+                var space = currentDate.Day.ToString().Length > 1
+                    ? RightOffsetTwoDigit + Delimiter
+                    : RightOffsetOneDigit + Delimiter;
+                if (currentDate.DayOfWeek.GetHashCode() == 0) {
+                    Console.Write(LeftOffset + currentDate.Day + space + "\n");
+                } else {
+                    Console.Write(LeftOffset + currentDate.Day + space);
+                    if (currentDate.DayOfWeek != DayOfWeek.Saturday) {
+                        countOfWorkingDays++;
+                    }
+                }
+
+                currentDate = currentDate.AddDays(1);
+            }
+        }
+
+        private static void SkipDays(DateTime dateTime) {
+            for (var step = 0; step < (dateTime.DayOfWeek.GetHashCode() - 1 + DayInWeek) % DayInWeek; step++) {
+                Console.Write("         ");
+            }
+        }
+
+        private static void ShowWorkingDays(int countOfWorkingDays) {
+            Console.WriteLine($"\nNumber of working boring days: {countOfWorkingDays}");
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            String[] fraza =
+            {
+                "ПОБЕДА ИЛИ СМЕРТЬ, угадывай дальше",
+                "я думал ты будешь угадывать бытсрей",
+                "у меня нет времени на игры, но ты молодец(нет)",
+                "даже мой 2-х летний брат угадывает быстрей..."
+            };
+
+            List<String> history = new List<string>();
+            int cout = 0;
+            int result = 0;
+            int a = 0;
+
+            Random rand = new Random();
+            int fraz = rand.Next(fraza.Length);
+            Console.WriteLine("Введите ваше имя");
+            String name = Console.ReadLine();
+            int answer = rand.Next(51);
+
+            Console.WriteLine("Хорошо " + name + " угадай число от 0 до 50");
+
+            DateTime date1 = DateTime.Now;
+            String s = Console.ReadLine();
+
+            if (s.Equals("q"))
+            {
+                Console.WriteLine("Извини, пока");
+            }
+            else
+            {
+                try
+                {
+                    a = Int32.Parse(s);
+                    result++;
+                    while (answer != a)
+                    {
+                        if (a < answer)
+                        {
+                            Console.WriteLine("число больше");
+                            history.Add(s + " меньше");
+                            cout++;
+                        }
+                        else if (a > answer)
+                        {
+                            Console.WriteLine("число меньше");
+                            history.Add(s + " больше");
+                            cout++;
+                        }
+
+                        if (cout % 4 == 0)
+                        {
+                            Console.WriteLine(name + "." + fraza[fraz]);
+                        }
+
+                        s = Console.ReadLine();
+
+                        if (s.Equals("q"))
+                        {
+                            Console.WriteLine("Извини, пока");
+                            break;
+                        }
+
+                        a = Int32.Parse(s);
+                        result++;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Нужно вводить число");
+                }
+
+                if (a == answer)
+                {
+                    Console.WriteLine("Хорошо, угадал");
+                    history.Add(s + " ровно");
+                    Console.WriteLine("Кол - во попыток" + " " + result);
+                    foreach (String h in history)
+                    {
+                        Console.WriteLine(h);
+                    }
+
+                    DateTime date2 = DateTime.Now;
+                    TimeSpan interval = date2 - date1;
+                    Console.WriteLine("Ты потратил" + " " + interval.ToString());
+                }
+            }
+        }
+    }
+}
